1c0b1a7cc5a3c67371721f3fbca37ce92610f351
Gabriel <gabrielfelipedealbuquerque19@hotmail.com>
2024-06-20 18:35:12 -0300

    Perfumarias

--- ProjetoA3/wwwroot/script.js
+++ ProjetoA3/wwwroot/script.js
@@ -2,16 +2,16 @@ let taskId = null;
 $(document).ready(function () {
     loadTasks();
 
-    $('#taskForm').on('submit', function (event) {
+    $("#taskForm").on("submit", function (event) {
         event.preventDefault();
         const task = {
-            title: $('#title').val(),
-            description: $('#description').val(),
-            responsible: $('#responsible').val(),
-            isCompleted: $('#isCompleted').is(':checked') || false
+            title: $("#title").val(),
+            description: $("#description").val(),
+            responsible: $("#responsible").val(),
+            isCompleted: $("#isCompleted").is(":checked") || false
         };
 
-        const deadDate = $('#deadDate').val();
+        const deadDate = $("#deadDate").val();
         if (deadDate) {
             task.deadDate = deadDate;
         }
@@ -26,20 +26,20 @@ $(document).ready(function () {
     });
 
     function formatDate(dateString) {
-        return moment(dateString).format('DD/MM/YYYY');
+        return moment(dateString).format("DD/MM/YYYY");
     }
 
     function loadTasks() {
-        $.getJSON('https://localhost:7047/api/tasks', function (tasks) {
-            $('#taskList').empty();
-            $('#taskListConc').empty();
+        $.getJSON("https://localhost:7047/api/tasks", function (tasks) {
+            $("#taskList").empty();
+            $("#taskListConc").empty();
 
             tasks.forEach(function (task) {
                 const formattedCreationDate = formatDate(task.creationDate);
-                const formattedDeadDate = task.deadDate ? formatDate(task.deadDate) : '';
+                const formattedDeadDate = task.deadDate ? formatDate(task.deadDate) : "";
 
                 if (task.isCompleted) {
-                    $('#taskListConc').append(`
+                    $("#taskListConc").append(`
                         <div class="card">
                             <span>#${task.id}</span>
                             <div class="card-header">
@@ -56,14 +56,15 @@ $(document).ready(function () {
                         </div>
                         `);
                 } else {
-                    $('#taskList').append(`
+                    $("#taskList").append(`
                         <div class="card">
                             <span>#${task.id}</span>
                             <div class="card-header">
                                 <div class="badge">
                                     <p>${task.responsible}</p>
                                 </div>
-                                <div class="card-options">
+                                <div class="card-options">                                
+                                    <button class="option-completed" onclick="markCompleted(${task.id})"><ion-icon name="checkmark-outline"></ion-icon></button>
                                     <button class="option-edit" onclick="editTask(${task.id})"><ion-icon name="create"></ion-icon></button>
                                     <button class="option-delete" onclick="deleteTask(${task.id})"><ion-icon name="trash"></ion-icon></button>
                                 </div>
@@ -90,19 +91,19 @@ $(document).ready(function () {
         task.creationDate = new Date();
 
         $.ajax({
-            url: 'https://localhost:7047/api/tasks',
-            type: 'POST',
-            contentType: 'application/json',
+            url: "https://localhost:7047/api/tasks",
+            type: "POST",
+            contentType: "application/json",
             data: JSON.stringify(task),
             success: function () {
                 loadTasks();
-                $('#taskForm')[0].reset();
-                $('#isCompletedContainer').empty();
-                toastr.success('Tarefa criada com sucesso!', 'Sucesso');
+                $("#taskForm")[0].reset();
+                $("#isCompletedContainer").empty();
+                toastr.success("Tarefa criada com sucesso!", "Sucesso");
             },
             error: function (xhr, status, error) {
-                console.error('Erro ao criar tarefa:', status, error);
-                toastr.error('Erro ao criar tarefa: ' + error, 'Erro');
+                console.error("Erro ao criar tarefa:", status, error);
+                toastr.error("Erro ao criar tarefa: " + error, "Erro");
             }
         });
     }
@@ -110,34 +111,50 @@ $(document).ready(function () {
     function updateTask(task) {
         $.ajax({
             url: `https://localhost:7047/api/tasks/${task.id}`,
-            type: 'PUT',
-            contentType: 'application/json',
+            type: "PUT",
+            contentType: "application/json",
             data: JSON.stringify(task),
             success: function () {
                 loadTasks();
-                $('#taskForm')[0].reset();
-                $('#isCompletedContainer').empty();
-                toastr.success('Tarefa atualizada com sucesso!', 'Sucesso');
+                $("#taskForm")[0].reset();
+                $("#isCompletedContainer").empty();
+                toastr.success("Tarefa atualizada com sucesso!", "Sucesso");
             },
             error: function (xhr, status, error) {
-                console.error('Erro ao atualizar tarefa:', status, error);
-                toastr.error('Erro ao atualizar tarefa: ' + error, 'Erro');
+                console.error("Erro ao atualizar tarefa:", status, error);
+                toastr.error("Erro ao atualizar tarefa: " + error, "Erro");
             }
         });
     }
-
+    window.markCompleted = function (id) {
+        $.ajax({
+            url: `https://localhost:7047/api/tasks/complete/${id}`,
+            type: "PUT",
+            success: function () {
+                loadTasks();
+                toastr.success("Tarefa concluída com sucesso!", "Sucesso");
+            },
+            error: function (xhr, status, error) {
+                console.error("Erro ao concluir tarefa:", status, error);
+                toastr.error("Erro ao concluir tarefa: " + error, "Erro");
+            }
+        });
+    }
     window.editTask = function (id) {
         $.getJSON(`https://localhost:7047/api/tasks/${id}`, function (task) {
             taskId = task.id;
-            $('#title').val(task.title);
-            $('#description').val(task.description);
-            $('#responsible').val(task.responsible);
-            const formattedDate = new Date(task.deadDate).toISOString().split('T')[0];
-            $('#deadDate').val(formattedDate);
+            $("#title").val(task.title);
+            $("#description").val(task.description);
+            $("#responsible").val(task.responsible);
 
-            $('#isCompletedContainer').html(`
+            if (task.deadDate) {
+                const formattedDate = new Date(task.deadDate).toISOString().split("T")[0];
+                $("#deadDate").val(formattedDate);
+            }
+
+            $("#isCompletedContainer").html(`
                 <label for="isCompleted">Concluída:</label>
-                <input type="checkbox" id="isCompleted" ${task.isCompleted ? 'checked' : ''}>
+                <input type="checkbox" id="isCompleted" ${task.isCompleted ? "checked" : ""}>
             `);
             openModal("edit");
         }).fail(function (jqxhr, textStatus, error) {
@@ -149,27 +166,27 @@ $(document).ready(function () {
 
     window.deleteTask = function (id) {
         Swal.fire({
-            title: 'Você tem certeza que deseja apagar essa tarefa?',
+            title: "Você tem certeza que deseja apagar essa tarefa?",
             text: "Não poderá ser desfeito",
-            icon: 'warning',
+            icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Sim, apagar!',
-            cancelButtonText: 'Cancelar'
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Sim, apagar!",
+            cancelButtonText: "Cancelar"
         }).then((result) => {
             if (result.isConfirmed) {
                 $.ajax({
                     url: `https://localhost:7047/api/tasks/${id}`,
-                    type: 'DELETE',
+                    type: "DELETE",
                     success: function () {
                         loadTasks();
 
-                        toastr.success('Tarefa apagada com sucesso!', 'Sucesso');
+                        toastr.success("Tarefa apagada com sucesso!", "Sucesso");
                     },
                     error: function (xhr, status, error) {
-                        console.error('Erro ao apagar tarefa:', status, error);
-                        toastr.error('Erro ao apagar tarefa: ' + error, 'Erro');
+                        console.error("Erro ao apagar tarefa:", status, error);
+                        toastr.error("Erro ao apagar tarefa: " + error, "Erro");
                     }
                 });
             }
@@ -178,27 +195,27 @@ $(document).ready(function () {
 
 
     function openModal(mode = "new") {
-        $('#taskModal').show();
+        $("#taskModal").show().css("display", "flex");
         if (mode == "new") {
             taskId = null;
-            $('#modal-header-title').text('Nova Tarefa');
-            $('#taskForm')[0].reset();
-            $('#isCompletedContainer').empty();
+            $("#modal-header-title").text("Nova Tarefa");
+            $("#taskForm")[0].reset();
+            $("#isCompletedContainer").empty();
         }
         else {
-            $('#modal-header-title').text('Editar Tarefa');
+            $("#modal-header-title").text("Editar Tarefa");
         }
     }
 
     function closeModal() {
-        $('#taskModal').hide();
+        $("#taskModal").hide();
     }
 
-    $('#open-modal').click(function () {
+    $("#open-modal").click(function () {
         openModal();
     });
 
-    $('#cancel-modal').click(function () {
+    $("#cancel-modal , .pelicula-modal").click(function () {
         closeModal();
     });
 
